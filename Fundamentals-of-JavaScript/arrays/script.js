542e0465a0bc964306f539f9f5e024b1c7feed43
Yogesh kumar <142971504+yogeshks6@users.noreply.github.com>
2024-10-15 22:51:17 +0530

    filter complete

--- Fundamentals-of-JavaScript/arrays/script.js
+++ Fundamentals-of-JavaScript/arrays/script.js
@@ -1,8 +1,22 @@
-var arr = [1, 2, 3, 4, 5];
-
 // foreach
+// var arr = [1, 2, 3, 4, 5];
 // arr.forEach(function (val) {
 //     console.log(val * 10)
 // })
 
 
+// map
+// var arr = [1, 2, 3, 4,5];
+// var ans = arr.map(function (val) {
+//     return val * 3;
+// })
+// console.log(ans);
+
+
+// filter
+// var arr = [1, 2, 3, 4, 5];
+// var ans = arr.filter(function (val) {
+//     if (val >= 3) { return true }
+//     else { return false }
+// })
+// console.log(ans)
