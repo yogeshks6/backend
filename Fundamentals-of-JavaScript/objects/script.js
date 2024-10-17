6479b76b3560fe1c02f7bf671b67944acb5939f3
Yogesh kumar <142971504+yogeshks6@users.noreply.github.com>
2024-10-17 23:11:06 +0530

    objects complete

--- Fundamentals-of-JavaScript/objects/script.js
+++ Fundamentals-of-JavaScript/objects/script.js
@@ -1,9 +1,10 @@
 // obejct is key value pairs
 
-var obj = {
-    name: "yogesh",
-    age: 12
-}
+// var obj = {
+//     name: "yogesh",
+//     age: 12
+// }
 // obj.name
 // obj['name']
 // Object.freeze(obj); "we do not change the value of obejct"
+// obj.age = 25; "we can change the value of object for this method"
