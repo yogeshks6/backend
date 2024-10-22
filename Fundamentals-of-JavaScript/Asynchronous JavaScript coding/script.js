b5717480592ce9b40d50abb95644f224e6fb90a6
Yogesh kumar <142971504+yogeshks6@users.noreply.github.com>
2024-10-22 12:26:52 +0530

    fetch name

--- Fundamentals-of-JavaScript/Asynchronous JavaScript coding/script.js
+++ Fundamentals-of-JavaScript/Asynchronous JavaScript coding/script.js
@@ -9,6 +9,6 @@
 // async function abcd() {
 //     var blob = await fetch(`https://randomuser.me/api/`);
 //     var ans = await blob.json();
-//     console.log(ans.results);
+//     console.log(ans.results[0].name);
 // }
 // abcd();
