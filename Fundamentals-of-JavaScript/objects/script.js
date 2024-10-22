824cf2e433e22eba357544449ed7ee4b311ed1fc
Yogesh kumar <142971504+yogeshks6@users.noreply.github.com>
2024-10-22 09:56:26 +0530

    function length

--- Fundamentals-of-JavaScript/objects/script.js
+++ Fundamentals-of-JavaScript/objects/script.js
@@ -8,3 +8,7 @@
 // obj['name']
 // Object.freeze(obj); "we do not change the value of obejct"
 // obj.age = 25; "we can change the value of object for this method" 
+
+function abcd(a, b, c, d) {
+
+}
